b011d251aaa8bdc8dc6f20dc4aa18675ade4fe5d
ranjanan <benditlikeranjan@gmail.com>
2018-11-06 16:38:02 -0500

    Add build scripts

---
+++ build/Circuitscape.jl
@@ -0,0 +1,83 @@
+using WebIO
+using Blink
+# import HttpParser
+using CircuitscapeUI
+# import ApplicationBuilder.App: change_dir_if_bundle
+import MacroTools
+import MbedTLS
+import Tachyons
+import InteractBase
+import InteractBulma
+import WebIO
+
+function change_dir_if_bundle()
+    # @show pwd()
+    # @show Base.julia_cmd()
+    cd(joinpath(split(string(Base.julia_cmd()))[1][2:end], ".."))
+    # @show pwd()
+end
+
+# Blink 
+Core.eval(Blink.AtomShell, :(_electron = "../Libraries/Julia.app/Contents/MacOS/Julia"))
+Core.eval(Blink.AtomShell, :(mainjs = "main.js"))
+Core.eval(Blink, :(buzz = "main.html"))
+Core.eval(Blink, :(resources = Dict("spinner.css" => "res/spinner.css", 
+                               "blink.js" => "res/blink.js", 
+                               "blink.css" => "res/blink.css", 
+                               "reset.css" => "res/reset.css")))
+
+# Mbedtls
+Core.eval(MbedTLS, :(libmbedcrypto = "../Libraries/libmbedcrypto"))
+Core.eval(MbedTLS, :(libmbedtls = "../Libraries/libmbedtls"))
+Core.eval(MbedTLS, :(libmbedx509 = "../Libraries/libmbedx509"))
+
+# HttpParser
+# eval(HttpParser, :(lib = "Libraries/libhttp_parser.dylib"))
+
+# Tachyons
+Core.eval(Tachyons, :(path = "tachyons.min.css"))
+
+# WebIO
+Core.eval(WebIO, :(bundlepath = "bundle.js"))
+Core.eval(WebIO, :(blinksetup = "blink_setup.js"))
+
+# InteractBase
+Core.eval(InteractBulma, :(all_js = "all.js"))
+Core.eval(InteractBulma, :(bulma_min_css = "bulma.min.css"))
+Core.eval(InteractBulma, :(bulma_slider_min_css = "bulma-slider.min.css"))
+Core.eval(InteractBulma, :(bulma_switch_min_css = "bulma-switch.min.css"))
+Core.eval(InteractBulma, :(bulma_checkradio_min_css = "bulma-checkradio.min.css"))
+
+# Vue 
+# eval(Vue, :(vue_js = "Resources/vue.js"))
+
+# CircuitscapeUI
+Core.eval(CircuitscapeUI, :(TESTPATH = "test/"))
+Core.eval(CircuitscapeUI, :(logo = "cs_logo.ico"))
+
+## MacroTools
+Core.eval(MacroTools, :(animals_file = "animals.txt"))
+
+Base.@ccallable function julia_main(args::Vector{String})::Cint
+    # ApplicationBuilder.App.change_dir_if_bundle()
+    # change_dir_if_bundle()
+    @show pwd()
+    @show Blink.AtomShell._electron
+    @show Tachyons.path
+    @show WebIO.bundlepath
+    @show WebIO.blinksetup
+    # @show HttpParser.lib
+    @show InteractBulma.bulma_min_css
+    # @show Vue.vue_js
+    @show InteractBulma.all_js
+    @show InteractBulma.bulma_slider_min_css
+    @show InteractBulma.bulma_switch_min_css
+    @show InteractBulma.bulma_checkradio_min_css
+    @show MacroTools.animals_file
+    w = CircuitscapeUI.run_ui()
+    @show AssetRegistry.registry
+    while Blink.active(w)
+        sleep(0.01)
+    end
+    return 0
+end
---
+++ build/build_script.jl
@@ -0,0 +1,41 @@
+using ApplicationBuilder
+# using BuildApp
+import Blink
+import Tachyons
+import WebIO
+import InteractBase
+import InteractBulma
+import Circuitscape
+import CircuitscapeUI
+import MbedTLS
+# import HttpParser
+
+dir(mod) = normpath(joinpath(dirname(pathof(mod)),".."))
+
+resources = 
+            [joinpath(dir(Blink), "src/AtomShell/main.js"), 
+             joinpath(dir(Blink), "src/content/main.html"), 
+             joinpath(dir(Blink), "res"), 
+             joinpath(dir(Tachyons), "assets/tachyons.min.css"), 
+             joinpath(dir(WebIO), "assets/webio/dist/bundle.js"), 
+             joinpath(dir(WebIO), "assets/providers/blink_setup.js"),
+             joinpath(dir(InteractBase), "assets/all.js"), 
+             joinpath(dir(InteractBulma), "assets/main.css"),
+             joinpath(dir(InteractBulma), "assets/bulma.min.css"),
+             joinpath(dir(InteractBulma), "assets/bulma-slider.min.css"),
+             joinpath(dir(InteractBulma), "assets/bulma-switch.min.css"),
+             joinpath(dir(InteractBulma), "assets/bulma-checkradio.min.css"),
+       #      joinpath(dir(Vue), "assets/vue.js"),
+             joinpath(dir(Circuitscape), "test"), 
+      #       joinpath(dir(MacroTools), "animals.txt"), 
+             joinpath(dir(CircuitscapeUI), "assets", "cs_logo.ico")
+            ]
+
+libraries = [joinpath(dir(Blink), "deps/Julia.app"), 
+             joinpath(dir(MbedTLS), "deps/usr/lib/libmbedcrypto.dylib"),
+             joinpath(dir(MbedTLS), "deps/usr/lib/libmbedtls.dylib"),
+             joinpath(dir(MbedTLS), "deps/usr/lib/libmbedx509.dylib")]
+             #joinpath(dir(HttpParser), "deps/usr/lib/libhttp_parser.dylib")]
+
+build_app_bundle("Circuitscape.jl", resources = resources, 
+                          libraries = libraries, icns_file = "/Users/ranjan/Repos/Circuitscape/circuitscape.icns")
